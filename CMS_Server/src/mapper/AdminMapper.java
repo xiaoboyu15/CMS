e5b3a761d78cf4b95b3d8b762710e36bf34677e8
1059748080 <1059748080@qq.com>
2018-08-27 23:07:18 +0800

    no message

--- CMS_Server/src/mapper/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import java.util.List;
-
-import entity.Admin;
-
-public interface AdminMapper {
-	public List<Admin> loginInAdmin();
-}
--- CMS_Server/src/mapper/ReporterMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import java.util.List;
-
-import entity.Reporter;
-
-public interface ReporterMapper {
-	public List<Reporter> loginInReporter();
-}
